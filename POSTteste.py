15f5a5376359f47f2c3a7f286270b8e78b9db8e1
mateusmota96 <100161766+mateusmota96@users.noreply.github.com>
2022-02-24 14:44:28 -0300

    Delete POSTteste.py

--- POSTteste.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-cliente = 'SERRANO'
-id = 1
-
-data = {"id": str(id)}
-# Aviso = 1 x = requests.post('http://sentinel-webtest.api.mateus.com.br/delnotify/postaviso', data)
-x = requests.post('http://sentinel-webtest.api.mateus.com.br/delnotify/delaviso', data) # Aviso = 0
-print(x.content)
